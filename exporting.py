44fa6383341bb505a7b028d38b486453bbd69b1d
Aishez singh <aishezsingh@gmail.com>
2023-11-14 22:57:46 +0530

    generating data : mapping done , Exporting : occupancy & counter

--- exporting.py
+++ exporting.py
@@ -2,6 +2,8 @@ import numpy as np
 import time
 import cv2
 from datetime import datetime
+import csv
+
 
 
 ARUCO_DICT = {
@@ -29,7 +31,7 @@ ARUCO_DICT = {
 }
 
 
-# changed format to calulate hourly_Slot_Set
+
 def merge_time_periods(time_list):
     if not time_list:
         return []
@@ -45,10 +47,20 @@ def merge_time_periods(time_list):
     return merged
 
 
+
+def export_to_csv(slot_data, filename):
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Slot ID', 'Currently Occupied', 'Slot Counter'])
+
+        for slot_id, data in enumerate(slot_data, start=1):
+            writer.writerow([slot_id, data['currently_occupied'], data['slot_counter']])
+
+
+
 def aruco_display(corners, ids, image, slot_timers, slot_occupancy, slot_records, final_time_list, slot_counters, hourly_slot_sets):
     if len(corners) > 0:
         slots = [0] * 5  # Initialize slots for different X-coordinate ranges
-
         current_time = datetime.now()
 
         for slot_index in range(5):
@@ -60,6 +72,40 @@ def aruco_display(corners, ids, image, slot_timers, slot_occupancy, slot_records
                 final_time_list[slot_index].append(
                     # Update final time list
                     (slot_timers[slot_index], exit_time))
+
+                # Extract hourly intervals from merged_time_list for the current slot
+                merged_time_list = final_time_list[slot_index]
+                for entry, exit_time in merged_time_list:
+                    start_hour = entry.hour
+                    end_hour = exit_time.hour
+
+                    if entry.minute > 0 or entry.second > 0:
+                        start_hour += 1
+
+                    if exit_time.minute > 0 or exit_time.second > 0:
+                        end_hour += 1
+
+                    # If start and end hour are the same, add the hour as a single element
+                    if start_hour == end_hour:
+                        hourly_slot_sets[slot_index].append([start_hour])
+                    else:
+                        # Add all hours in between
+                        hours_in_interval = list(range(start_hour, end_hour))
+                        # Exclude the last hour if the minute is 00 for the end hour
+                        if exit_time.minute == 0:
+                            hours_in_interval = hours_in_interval[:-1]
+
+                        hourly_slot_sets[slot_index].append(hours_in_interval)
+
+                # Recalculate the activated slots for the last interval in the merged list
+                last_entry, last_exit = merged_time_list[-1]
+                start_hour = last_entry.hour
+                end_hour = last_exit.hour
+                if last_entry.minute > 0 or last_entry.second > 0:
+                    start_hour += 1
+                if last_exit.minute > 0 or last_exit.second > 0:
+                    end_hour += 1
+                hourly_slot_sets[slot_index] = [list(range(start_hour, end_hour + 1))]
 
                 slot_occupancy[slot_index] = "Not Occupied"
                 slot_timers[slot_index] = None
@@ -102,6 +148,15 @@ def aruco_display(corners, ids, image, slot_timers, slot_occupancy, slot_records
                     final_time_list[slot_index].append(
                         (slot_timers[slot_index], exit_time))  # Update final time list
 
+                    # Calculate the hourly slots during which the slot was occupied
+                    start_hour = slot_timers[slot_index].hour
+                    end_hour = exit_time.hour
+                    if slot_timers[slot_index].minute > 0 or slot_timers[slot_index].second > 0:
+                        start_hour += 1
+                    if exit_time.minute > 0 or exit_time.second > 0:
+                        end_hour += 1
+                    hourly_slots = set(range(start_hour, end_hour))
+                    hourly_slot_sets[slot_index].update(hourly_slots)
 
                     slot_occupancy[slot_index] = "Not Occupied"
                     slot_timers[slot_index] = None
@@ -119,10 +174,45 @@ def aruco_display(corners, ids, image, slot_timers, slot_occupancy, slot_records
                     # Update final time list
                     (slot_timers[slot_index], exit_time))
 
+                # Extract hourly intervals from merged_time_list for the current slot
+                merged_time_list = final_time_list[slot_index]
+                for entry, exit_time in merged_time_list:
+                    start_hour = entry.hour
+                    end_hour = exit_time.hour
+
+                    if entry.minute > 0 or entry.second > 0:
+                        start_hour += 1
+
+                    if exit_time.minute > 0 or exit_time.second > 0:
+                        end_hour += 1
+
+                    # If start and end hour are the same, add the hour as a single element
+                    if start_hour == end_hour:
+                        hourly_slot_sets[slot_index].append([start_hour])
+                    else:
+                        # Add all hours in between
+                        hours_in_interval = list(range(start_hour, end_hour))
+                        # Exclude the last hour if the minute is 00 for the end hour
+                        if exit_time.minute == 0:
+                            hours_in_interval = hours_in_interval[:-1]
+
+                        hourly_slot_sets[slot_index].append(hours_in_interval)
+
+                # Recalculate the activated slots for the last interval in the merged list
+                last_entry, last_exit = merged_time_list[-1]
+                start_hour = last_entry.hour
+                end_hour = last_exit.hour
+                if last_entry.minute > 0 or last_entry.second > 0:
+                    start_hour += 1
+                if last_exit.minute > 0 or last_exit.second > 0:
+                    end_hour += 1
+                hourly_slot_sets[slot_index] = [list(range(start_hour, end_hour + 1))]
+
                 slot_occupancy[slot_index] = "Not Occupied"
                 slot_timers[slot_index] = None
 
         return image, None, None, final_time_list, hourly_slot_sets
+
 
 
 aruco_type = "DICT_4X4_250"
@@ -133,20 +223,17 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-slot_timers = [None] * 5  # Initialize slot timers
-slot_occupancy = ["Not Occupied"] * 5  # Initialize slot occupancy status
-slot_records = [[] for _ in range(5)]  # Initialize slot_records list
-# Initialize final time list for each slot
+slot_timers = [None] * 5
+slot_occupancy = ["Not Occupied"] * 5
+slot_records = [[] for _ in range(5)]
 final_time_list = [[] for _ in range(5)]
 slots = [0] * 5
 slot_counters = [0] * 5
-# Initialize hourly slot sets for each slot
 hourly_slot_sets = [[] for _ in range(5)]
 
-
 start_time = time.time()
-slot_reset_interval = 5     # Reset slots after 5 seconds of inactivity
-print_interval = 10         # Print slot contents every 10 seconds
+slot_reset_interval = 5
+print_interval = 10
 last_print_time = start_time
 
 while cap.isOpened():
@@ -156,12 +243,10 @@ while cap.isOpened():
     height = int(width * (h / w))
     img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
 
-    corners, ids, _ = cv2.aruco.detectMarkers(
-        img, arucoDict, parameters=arucoParams)
+    corners, ids, _ = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
 
     if len(corners) > 0:
         detected_markers, updated_slots, updated_occupancy, final_time_list, hourly_slot_sets = aruco_display(
-            # Pass slot_counters and hourly_slot_sets as arguments
             corners, ids, img, slot_timers, slot_occupancy, slot_records, final_time_list, slot_counters, hourly_slot_sets)
         if updated_slots is not None:
             slots = updated_slots
@@ -179,53 +264,25 @@ while cap.isOpened():
     if current_time - last_print_time >= print_interval:
         last_print_time = current_time
         print("Slot contents after {} seconds:".format(print_interval))
+
+        slot_data_for_export = []
         for i in range(5):
-            merged_time_list = merge_time_periods([(entry.strftime(
-                '%Y-%m-%d %H:%M:%S'), exit.strftime('%Y-%m-%d %H:%M:%S')) for entry, exit in final_time_list[i]])
-            slot_counters[i] = len(merged_time_list)  # Update the slot counter
+            merged_time_list = merge_time_periods([(entry.strftime('%Y-%m-%d %H:%M:%S'), exit.strftime('%Y-%m-%d %H:%M:%S')) for entry, exit in final_time_list[i]])
+            slot_counters[i] = len(merged_time_list)
 
+            # Append slot data for export
+            slot_data_for_export.append({
+                'currently_occupied': slot_occupancy[i],
+                'slot_counter': slot_counters[i]
+            })
 
-
-            if len(merged_time_list) > 0:
-                if len(merged_time_list) != len(hourly_slot_sets[i]):
-                    # If size has changed, append a new set from the last interval
-                    last_interval = merged_time_list[-1]
-                    start_hour = datetime.strptime(last_interval[0], '%Y-%m-%d %H:%M:%S').hour
-                    end_hour = datetime.strptime(last_interval[1], '%Y-%m-%d %H:%M:%S').hour + 1
-                    hours_in_last_interval = list(range(start_hour, end_hour))
-                    # Exclude the last hour if the minute is 00 for the end hour
-                    if datetime.strptime(last_interval[1], '%Y-%m-%d %H:%M:%S').minute == 0:
-                        hours_in_last_interval = hours_in_last_interval[:-1]
-
-                    hourly_slot_sets[i].append(set(hours_in_last_interval))
-                else:
-                    # If size has not changed, recalculate set for the last interval
-                    last_interval = merged_time_list[-1]
-                    start_hour = datetime.strptime(last_interval[0], '%Y-%m-%d %H:%M:%S').hour
-                    end_hour = datetime.strptime(last_interval[1], '%Y-%m-%d %H:%M:%S').hour + 1
-                    hours_in_last_interval = list(range(start_hour, end_hour))
-                    # Exclude the last hour if the minute is 00 for the end hour
-                    if datetime.strptime(last_interval[1], '%Y-%m-%d %H:%M:%S').minute == 0:
-                        hours_in_last_interval = hours_in_last_interval[:-1]
-
-                    # Update the last set in the hourly_slot_sets
-                    hourly_slot_sets[i][-1] = set(hours_in_last_interval)
-
-
-
-
-            # Print the updated slot counter
-            # Print the updated slot counter and occupancy
             print(f"Slot {i + 1} Counter:", slot_counters[i])
             print(f"Slot {i + 1} Occupancy:", slot_occupancy[i])
-
-            # Print the hourly slot set for the current slot
             print(f"Slot {i + 1} Hourly Set:", hourly_slot_sets[i])
+            print("Final Time List for Slot {}: {}".format(i + 1, merged_time_list))
 
-            # print("In and Out Info for Slot {}: {}".format(
-            #     i + 1, slot_records[i]))
-            print("Final Time List for Slot {}: {}".format(
-                i + 1, merged_time_list))
+        # Export slot data to CSV
+        export_to_csv(slot_data_for_export, 'slot_data.csv')
 
         print("\n")
 
