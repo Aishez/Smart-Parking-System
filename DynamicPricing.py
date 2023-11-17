60d76965a89f8308de26000d7dad75e607dd6aff
khushisingh4218 <95536940+khushisingh4218@users.noreply.github.com>
2023-11-17 16:09:03 +0530

    updated version

--- DynamicPricing.py
+++ DynamicPricing.py
@@ -1,40 +1,48 @@
+
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-data = {
-    'DayOfWeek': np.random.randint(1, 8, 100),  
-    'TimeOfDay': np.random.randint(1, 4, 100), 
-    'IsSpecialDay': np.random.randint(0, 2, 100),  
-    'HistoricalReservations': np.random.randint(0, 100, 100), 
-    'Occupancy': np.random.randint(0, 50, 100),  # Real-time
-    'TotalAvailableSpots': np.random.randint(50, 100, 100),  # Total available spots
-    'PercentageAvailableSpots': np.random.uniform(0.5, 1.0, 100),  
-    'CustomerType': np.random.randint(0, 2, 100),  
-    'MembershipStatus': np.random.randint(0, 2, 100),  
-    'DynamicPrice': np.random.randint(10, 50, 100)
-}
+# Load data from CSV file
+file_path = 'data.csv'  # Replace with the actual path to your CSV file
+df = pd.read_csv(file_path)
 
-
-df = pd.DataFrame(data)
-
-X = df[['DayOfWeek', 'TimeOfDay', 'IsSpecialDay', 'HistoricalReservations', 'Occupancy', 'TotalAvailableSpots', 'PercentageAvailableSpots', 'CustomerType', 'MembershipStatus']]
+# Assuming your CSV file has columns similar to the synthetic data
+X = df[['DayOfWeek', 'TimeOfDay', 'IsSpecialDay', 'SlotNumber','Reservations', 'TotalAvailableSpots', 'PercentageAvailableSpots', 'CustomerType', 'MembershipStatus']]
 y = df['DynamicPrice']
 
-# Splitting the data into training and testing sets (80% training, 20% testing)
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Create and train the model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+# Make predictions on the test set
 y_pred = model.predict(X_test)
 
-
+# Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
 
-#need to access this in real time
-new_reservation_features = np.array([[4, 2, 0, 30, 25, 80, 0.75, 1, 1]])
+
+# Now, you can use the trained model to make predictions on new data
+# For example, assuming 'new_reservation_features' is a new set of features
+new_reservation_features = pd.DataFrame({
+    'DayOfWeek': [2],
+    'TimeOfDay': [1],
+    'IsSpecialDay': [1],
+    'SlotNumber' : [2],
+    'Reservations': [1],
+    'TotalAvailableSpots': [5],
+    'PercentageAvailableSpots': [1],
+    'CustomerType': [0],
+    'MembershipStatus': [2]
+})
+
 predicted_price = model.predict(new_reservation_features)
+new_reservation_features['DynamicPrice'] = predicted_price.flatten()
+
+new_reservation_features.to_csv(file_path, mode='a', header=False, index=False)
+
 print(f"Predicted Price: ${predicted_price[0]:.2f}")
